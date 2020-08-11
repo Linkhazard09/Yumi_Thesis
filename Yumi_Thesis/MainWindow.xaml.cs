242f18f11f5d662e15a19672cbdd2f29305bf60c
MATTHEW PALADINE RIVERA <mpprivera@mymail.mapua.edu.ph>
2020-08-12 02:14:27 +0800

    Disabled Resizing for all forms

--- Yumi_Thesis/MainWindow.xaml.cs
+++ Yumi_Thesis/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Windows.Markup;
+using System.Globalization;
 
 
 namespace Yumi_Thesis
@@ -541,5 +543,64 @@ namespace Yumi_Thesis
 
 
         }
+    }
+
+
+    [ValueConversion(typeof(string), typeof(string))]
+    public class RatioConverter : MarkupExtension, IValueConverter
+    {
+        private static RatioConverter _instance;
+
+        public RatioConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        { // do not let the culture default to local to prevent variable outcome re decimal syntax
+            double size = System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return size.ToString("G0", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { // read only converter...
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new RatioConverter());
+        }
+
+    }
+
+
+
+
+}
+
+namespace MyApp.Tools
+{
+
+    [ValueConversion(typeof(string), typeof(string))]
+    public class RatioConverter : MarkupExtension, IValueConverter
+    {
+        private static RatioConverter _instance;
+
+        public RatioConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        { // do not let the culture default to local to prevent variable outcome re decimal syntax
+            double size = System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return size.ToString("G0", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { // read only converter...
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new RatioConverter());
+        }
+
     }
 }
---
+++ Yumi_Thesis/PreferenceTestExperiment.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MatchingTestExperiment
+{
+    /// <summary>
+    /// Interaction logic for PreferenceTestExperiment.xaml
+    /// </summary>
+    public partial class PreferenceTestExperiment : Window
+    {
+        public string ParticipantName ="Matthew Paladine Rivera";
+        //public string[] Sequence = new string[105];
+        public  int SequenceCtr = 0;
+        public string[] Sequence = { "/Images/A01.jpg", "/Images/D21.jpg" , "/Images/C17.jpg", "/Images/B09.jpg", "/Images/E15.jpg" };
+        public int Like=0, Look = 0, Effective = 0, Clarity = 0, Strength = 0, Familiar = 0, Complex = 0, Organize = 0, Clutter = 0, Recognize = 0, Abstract = 0, Compatible = 0;
+
+
+
+
+        public PreferenceTestExperiment()//Declare Sequence here
+        {
+            InitializeComponent();
+            FunctionImage.Source = new BitmapImage(new Uri(Sequence[SequenceCtr], UriKind.Relative));
+            SequenceCtr++;
+            string ImgSource = Sequence[SequenceCtr];
+
+            Function_Name.Text = Labeltest(ImgSource);
+
+
+        }
+
+        private void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (SequenceCtr > 4)
+                SequenceCtr = 0;
+
+
+            FunctionImage.Source = new BitmapImage(new Uri(Sequence[SequenceCtr], UriKind.Relative));
+            string ImgSource = Sequence[SequenceCtr];
+            SequenceCtr++;
+            Function_Name.Text = Labeltest(ImgSource);
+
+
+
+        }
+
+        private void Label_MouseEnter(object sender, MouseEventArgs e)
+        {
+            NextLabel.Foreground = Brushes.LightGray;
+        }
+
+        private void Label_MouseLeave(object sender, MouseEventArgs e)
+        {
+           NextLabel.Foreground = Brushes.Black;
+        }
+
+        private string Labeltest(string ImgSource)
+        {
+            //Tried using switch but apparently cant be used that way :/
+            if (ImgSource.Contains("01"))
+                return "Fever";
+            else if (ImgSource.Contains("02"))
+                return "Shortness In Breath";
+            else if (ImgSource.Contains("03"))
+                return "Cough or Cold";
+            else if (ImgSource.Contains("04"))
+                return "Cough or Cold";
+            else if (ImgSource.Contains("05"))
+                return "Wash hands";
+            else if (ImgSource.Contains("06"))
+                return "Drink and Eat Healthy";
+            else if (ImgSource.Contains("07"))
+                return "Consult Doctor/ Seek Medical Help";
+            else if (ImgSource.Contains("08"))
+                return "Avoid People With Flu-Like Symptoms";
+            else if (ImgSource.Contains("09"))
+                return "Cover when Coughing or Sneezing";
+            else if (ImgSource.Contains("10"))
+                return "Avoid Contact with Animals";
+            else if (ImgSource.Contains("11"))
+                return "Get Information from Trusted Sources";
+            else if (ImgSource.Contains("12"))
+                return "Wear Face Masks";
+            else if (ImgSource.Contains("13"))
+                return "Avoid Crowded Places Limit Social Gatherings";
+            else if (ImgSource.Contains("14"))
+                return "Dispose Tissue and Face Masks on Waste Cans";
+            else if (ImgSource.Contains("15"))
+                return "Use Hand Sanitizers";
+            else if (ImgSource.Contains("16"))
+                return "Avoid Travelling to Places with Known Cases";
+            else if (ImgSource.Contains("17"))
+                return "Avoid Touching Your Face";
+            else if (ImgSource.Contains("18"))
+                return "Wash Clothes Properly";
+            else if (ImgSource.Contains("19"))
+                return "Decontaminate or Disinfect";
+            else if (ImgSource.Contains("20"))
+                return "Home Quarantine/ Stay at Home";
+            else if (ImgSource.Contains("21"))
+                return "Social Distancing";
+            else
+                return "";
+
+
+        }
+
+
+
+
+
+    }
+}
